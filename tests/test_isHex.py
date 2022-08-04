cd746636ff1fab0f2055c8941f86bcc2f664d8bb
Xhelphin <jaxk.programmer@gmail.com>
2022-08-04 14:27:38 +0100

    Add new functions and relevant tests

--- tests/test_isHex.py
+++ tests/test_isHex.py
@@ -1,4 +1,4 @@
-from isHex import isHex
+from isHex import isHex, isHexLower, isHexUpper
 
 def test_isHex_valid_lowercase_chars():
     assert isHex("abcdef") == True
@@ -20,3 +20,49 @@ def test_isHex_invalid_chars():
 
 def test_isHex_invalid_mixed_numbers_and_chars():
     assert isHex("97863hgfe347") == False
+
+##
+
+def test_isHexLower_valid_lowercase_chars():
+    assert isHexLower("abcdef") == True
+
+def test_isHexLower_valid_uppercase_chars():
+    assert isHexLower("ABCDEF") == False
+
+def test_isHexLower_valid_mixed_chars():
+    assert isHexLower("aBcDeF") == False
+
+def test_isHexLower_valid_numbers():
+    assert isHexLower("1234567890") == True
+
+def test_isHexLower_mixed_numbers_and_chars():
+    assert isHexLower("1234567890aBcDeF") == False
+
+def test_isHexLower_invalid_chars():
+    assert isHex("abcdefg") == False
+
+def test_isHexLower_invalid_mixed_numbers_and_chars():
+    assert isHexLower("97863hgfe347") == False
+    
+##
+
+def test_isHexUpper_valid_lowercase_chars():
+    assert isHexUpper("abcdef") == False
+
+def test_isHexUpper_valid_uppercase_chars():
+    assert isHexUpper("ABCDEF") == True
+
+def test_isHexUpper_valid_mixed_chars():
+    assert isHexUpper("aBcDeF") == False
+
+def test_isHexUpper_valid_numbers():
+    assert isHexUpper("1234567890") == True
+
+def test_isHexUpper_mixed_numbers_and_chars():
+    assert isHexUpper("1234567890aBcDeF") == False
+
+def test_isHexUpper_invalid_chars():
+    assert isHexUpper("abcdefg") == False
+
+def test_isHexUpper_invalid_mixed_numbers_and_chars():
+    assert isHexUpper("97863hgfe347") == False
