23da5175639b102845fb84a3d5d7bcb1cfbb94be
Xhelphin <jaxk.programmer@gmail.com>
2022-08-04 14:38:25 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="isHex",
-    version="1.0.0",
+    version="1.0.1",
     description="Simple Python package to check if string is valid hexadecimal.",
     py_modules=["isHex"],
     package_dir={'': 'src'},
