e399d98ddf96b20f3d6a2ced79b870239949d9ae
Xhelphin <32981635+Xhelphin@users.noreply.github.com>
2023-02-28 14:36:56 +0000

    Change isHexUpper from @wjmelements suggestion

--- src/isHex.py
+++ src/isHex.py
@@ -35,8 +35,10 @@ def isHexUpper(string):
     """
     Returns True if string is valid hexidecimal with uppercase characters, else returns False.
     """
-    validCharacters = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
     for character in string:
-        if character not in validCharacters:
-            return False
+        if '0' <= character <= '9':
+            continue
+        if 'A' <= character <= 'F':
+            continue
+        return False
     return True
